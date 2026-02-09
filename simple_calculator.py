de8840475c9bcaecd16ffb705a85c1210373f52e
Nadhif Ibrahim <mnadhifibr@email.com>
2026-02-09 16:44:44 +0700

    ubah variabel

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,6 +1,6 @@
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    def add(self, angka1, angka2):
+        return angka1 + angka2
     def subtract(self, a, b):
         return a - b
     def multiply(self, a, b):
